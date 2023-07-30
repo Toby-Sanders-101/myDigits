e342225034ca2ecd588454bf96b3a2002791a8f6
Toby Sanders <136257434+Toby-Sanders-101@users.noreply.github.com>
2023-07-30 21:47:51 +0100

    Update useMyDigitsReader.py comments

--- useMyDigitsReader.py
+++ useMyDigitsReader.py
@@ -5,73 +5,61 @@ from csv import reader
 import os
 import myDigitsReaderHelp as hlp
 
-size = "40x27"
+size = "40x27" #these are the dimensions of the images that you want to process
+total_pixels = 40*27
 
-parameters = hlp.readParams(size)
-qd = hlp.getQD(size)
+parameters = hlp.readParams(size) #this reads the parameters stored in a file
+qd = hlp.getQD(size) #the size used should correspond to a 'quality drop'. This is the decrease in resolution from your camera's quality to your size/quality eg
+			#a drop from dimensions 480x320 -> 40x27 is division by 12. 480/12=40 and 320/12=27. Therefore qd = 12
+outRange = 10 #outRange is the number of unique possible outputs. There are 10 digits so my range would be 10 however this may need altering if you want to change what content the program reads
 
-#x,y,_,_,_ = hlp.readDigits(size="120x80")
-
-def linear_forward(A, W, b):
-	Z = np.dot(W,A)+b
-	assert(Z.shape == (W.shape[0], A.shape[1]))
-	return Z
-
-def sigmoid(Z):
-	return 1/(1+np.exp(-Z))
-
-def relu(Z):
-	return Z*(Z>0)
-
-def linear_activation_forward(A_prev,W,b,activation):
-	Z = linear_forward(A_prev,W,b)
+def forward_one_layer(A_prev,W,b,activation): #calculates the outputs of a layer based on: its weights, biases, activation function and inputs (from the previous layer)
+	Z = np.dot(W,A_prev)+b
 	if activation == "sigmoid":
-		A = sigmoid(np.clip(Z,-25,25))
+		A = 1/(1+np.exp(-(np.clip(Z,-25,25))))
 	elif activation == "relu":
-		A = relu(Z)
+		A = max([0,Z])
 	return A
 
-def L_model_forward(X, parameters):
+def forward_all_layers(X, parameters): #moves through all the layers of the network and makes predictions using the inputs (X) and parameters
 	A = X
 	L = len(parameters) // 2
 	for l in range(1, L):
 		A_prev = A 
-		A = linear_activation_forward(A_prev,parameters['W'+str(l)],parameters['b'+str(l)],"relu")
-	AL = linear_activation_forward(A,parameters['W'+str(L)],parameters['b'+str(L)],"sigmoid")
+		A = forward_one_layer(A_prev,parameters['W'+str(l)],parameters['b'+str(l)],"relu")
+	AL = forward_one_layer(A,parameters['W'+str(L)],parameters['b'+str(L)],"sigmoid")
 	return AL
 
-def predict(X,parameters):
-	al=L_model_forward(X,parameters)
+def predict(X,parameters): #makes a prediction for input data X. returns the most likely digit and its confidence
+	al=forward_all_layers(X,parameters)
 	preds = []
 	confs = []
 	for i in al.T:
-		mark = 0
-		maxi = 0
-		for j in range(4):
-			if maxi<i[j]:
+		mark = 0 #index of highest confidence
+		maximum = 0 #highest confidence
+		for j in range(outRange):
+			if maximum<i[j]:
 				mark = j
-				maxi = i[j]
+				maximum = i[j]
 		preds.append(mark)
 		confs.append(np.max(i)/np.sum(i))
 	return preds, confs
 
-#print(predict(x,parameters)[0])
-
-cam = cv2.VideoCapture(0)#dim=480x640
+cam = cv2.VideoCapture(0) #initialises the camera
 ptime = time.time()
 while True:
-	ret, image = cam.read()
-	image = hlp.shrinkData(image,size)
-	x = image.flatten().reshape(1,(round(480/qd)*round(320/qd)))
-	image = 255*cv2.resize(image,(0,0),fx=qd,fy=qd)
-	preds,confs = predict(x.T,parameters)
-	image = cv2.putText(image,str(preds[0])+"    "+str(round(confs[0]*100,1))+"%",(75,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 0),2,cv2.LINE_AA)
+	ret, image = cam.read() #take a photo
+	image = hlp.shrinkData(image,size) #decrease the quality of the photo. I cropped it, shrunk it and reduced it to black and white (0 or 1)
+	x = image.flatten().reshape(1,total_pixels) #reshape the image so that the model can process it
+	image = 255*cv2.resize(image,(0,0),fx=qd,fy=qd) #this increases the size and changes it from 0's or 1's into 0's or 255's so that it is comprehendable
+	preds,confs = predict(x.T,parameters) #use the model to make a prediction with a certain confidence
+	image = cv2.putText(image,str(preds[0])+"    "+str(int(confs[0]*100))+"%",(75,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 0),2,cv2.LINE_AA) #output the prediction and confidence
 	ctime = time.time()
-	image = cv2.putText(image,"FPS: "+str(int(1/(ctime-ptime))),(75,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 0),2,cv2.LINE_AA)
-	cv2.imshow('Imagetest',image)
-	k = cv2.waitKey(10)
+	image = cv2.putText(image,"FPS: "+str(int(1/(ctime-ptime))),(75,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 0),2,cv2.LINE_AA) #output the fps
+	cv2.imshow('Imagetest',image) #output the normal sized image
+	k = cv2.waitKey(1) #doesn't return -1 if a key is pressed
 	ptime = time.time()
-	if k != -1:
+	if k != -1: #exits program
 		break
 cam.release()
 cv2.destroyAllWindows()
